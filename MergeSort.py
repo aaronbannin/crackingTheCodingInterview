c57a0fb3a4cddde058fedfe827097493fc6c3a92
Aaron Bannin <aaronbannin@gmail.com>
2017-04-13 15:30:30 -0700

    remove excess code

--- MergeSort.py
+++ MergeSort.py
@@ -1,39 +1,23 @@
 def merge_sort(input_array):
-    length = len(input_array)
-    # print('length={} array={}'.format(length, input_array))
-    if length > 2:
-        return_value = []
-        half = int(length/2)
+    if len(input_array) is 0 or len(input_array) is 1:
+        return input_array
+    else:
+        half = int(len(input_array)/2)
         left = merge_sort(input_array[:half])
         right = merge_sort(input_array[half:])
-        # print('length={} half={} left={} right={} input_array={}'.format(length, half, left, right, input_array))
+
+        return_value = []
         left_index = 0
         right_index = 0
-        while left_index < len(left) or right_index < len(right):
+        while True:
             if left_index >= len(left):
-                return_value = return_value + right[right_index:]
-                break
+                return return_value + right[right_index:]
             elif right_index >= len(right):
-                return_value = return_value + left[left_index:]
-                break
-            elif left[left_index] < right[right_index]:
-                return_value.append(left[left_index])
-                left_index += 1
+                return return_value + left[left_index:]
             else:
-                return_value.append(right[right_index])
-                right_index += 1
-        return return_value
-    else:
-        for i in range(len(input_array)):
-            if len(input_array) is 0 or len(input_array) is 1:
-                # print('returning {}'.format(input_array))
-                return input_array
-            elif len(input_array) == 2:
-                if input_array[0] > input_array[1]:
-                    # print('returning {}'.format([input_array[1], input_array[0]]))
-                    return [input_array[1], input_array[0]]
+                if left[left_index] < right[right_index]:
+                    return_value.append(left[left_index])
+                    left_index += 1
                 else:
-                    # print('returning {}'.format(input_array))
-                    return input_array
-            else:
-                raise ValueError('Array too large input_array={}'.format(input_array))
+                    return_value.append(right[right_index])
+                    right_index += 1
